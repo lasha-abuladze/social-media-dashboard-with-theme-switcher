22935f045852c6e11bb86e48539eb35457182541
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2024-10-09 13:26:08 +0400

    add general overviews in main section

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ const body = document.querySelector(`body`);
 const header = document.querySelector(`header`);
 const text = document.querySelectorAll(`.white-color`);
 const circle = document.querySelector(`.circle`);
-
 const switchMode = document.querySelector(`.switch-div`);
+const overviewContainer = document.querySelectorAll(`.general-overview--div`);
+
+const numbers = document.querySelectorAll(`.number`);
+const follSubSpan = document.querySelectorAll(`.foll-sub--numb`);
+const arrowIcons = document.querySelectorAll(`.arrow-icon`);
+
+
+
+
+numbers.forEach((el, i) => {
+    if(Number(el.textContent) > 0) {
+        follSubSpan[i].style.color = `#1EB589`
+        arrowIcons[i].src = `./images/icon-up.svg`
+
+    } else {
+        numbers[i].textContent = `${Math.abs(parseInt(numbers[i].textContent))}`
+        follSubSpan[i].style.color = `red`
+        arrowIcons[i].src = `./images/icon-down.svg`
+    }
+})
 
 
 
@@ -19,4 +38,5 @@ switchMode.addEventListener(`click`, () => {
     switchMode.classList.toggle(`switch-div--dark`);
     
     text.forEach(el => el.classList.toggle(`text--white`));
+    overviewContainer.forEach(el => el.classList.toggle(`general-overview--div--dark`))
 })
